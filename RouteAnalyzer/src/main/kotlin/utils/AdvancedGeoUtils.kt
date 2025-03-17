82792593d532f9f03d98e89133922d0401ec4f88
Amir Zand <amirzv@outlook.com>
2025-03-17 10:52:56 +0100

    docs: add TODOs for advanced route analysis

---
+++ RouteAnalyzer/src/main/kotlin/utils/AdvancedGeoUtils.kt
@@ -0,0 +1,54 @@
+package g11.utils
+
+
+/**
+ * README: These are suggested feature that we can implement as extra features.
+ * Feel free to add more features if you have something in mind.
+ */
+
+
+/**
+ * TODO: Implement a function to detect intersection between two geofence
+ * It should receive 6 parameters:
+ *  - latitude1: Double
+ *  - longitude1: Double
+ *  - radius1: Double
+ *  - latitude2: Double
+ *  - longitude2: Double
+ *  - radius2: Double
+ *  And return a boolean indicating if the two geofences intersect.
+ */
+
+
+/**
+ * TODO: Implement a function to calculate Total Distance Traveled
+ * It should receive a list of waypoints and return the total distance traveled in kilometers.
+ */
+
+
+/**
+ * TODO: Implement a function to find Entry & Exit Points of Geofence
+ * It should receive 4 parameters:
+ * - waypoints: List<Waypoint>
+ *     A list of waypoints representing the route.
+ *     The list is ordered by timestamp.
+ * - centerLatitude: Double
+ * - centerLongitude: Double
+ * - radius: Double
+ * And return a Pair of Waypoint objects representing the entry and exit points of the geofence.
+ */
+
+
+/**
+ * TODO: Implement a function to calculate average speed and maximum speed between waypoints.
+ * It should receive a list of waypoints and return a Pair of Doubles representing
+ * the average speed and maximum speed.
+ */
+
+
+
+/**
+ * TODO: Implement a function to identify when the user was stationary (speed near zero).
+ * It should receive a list of waypoints and return a list of Pair of Doubles representing
+ * the start and end timestamps of each stationary period.
+ */
